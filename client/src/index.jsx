6a942f0c650beab4bccc9e9665556d46fc5be1db
viktorlv30 <viktor.lv.30@gmail.com>
2017-01-05 16:28:12 +0200

    add react-redux to project

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,36 @@
  */
 import React from 'react';
 import ReactDom from 'react-dom';
-import Voting from './components/Voting';
+import { Router, Route, hashHistory } from 'react-router';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+import App from './components/App';
+import { VotingContainer } from './components/Voting';
+import Results from './components/Results';
 // import './style.css'
 
-const pair = ['Trainspotting', '28 Days Later'];
+const store = createStore(reducer);
+store.dispatch({
+    type: 'SET_STATE',
+    state: {
+        vote: {
+            pair: ['Sunshine', '28 Days Later'],
+            tally: { Sunshine: 2 }
+        }
+    }
+});
+
+// const pair = ['Trainspotting', '28 Days Later'];
+
+const routes = <Route component={ App }>
+    <Route path="/" component={ VotingContainer } />
+    <Route path="/results" components={ Results } />
+</Route>;
 
 ReactDom.render(
-    <Voting pair={pair} winner='Trainspotting' />,
+    <Provider store={ store }>
+        <Router history={ hashHistory }>{ routes }</Router>
+    </Provider>,
     document.getElementById('app')
 );
